186518da43d717663a4fa4b5dcd2df48f048fcc9
Kangeyan Passoubady <kpassoubady@gmail.com>
2026-02-19 23:38:25 -0800

    assets added

--- configs/run-book-builder.sh
+++ configs/run-book-builder.sh
@@ -21,3 +21,13 @@ bookbuilder build \
   --temp /Users/kangs/Downloads/book \
   --output /Users/kangs/github/bookbuilder/temp/tamil-prompt-engineering-book-show-case.epub \
   --format=epub
+
+// running test build for quick test config
+
+bookbuilder build \
+  --root /Users/kangs/github/tamil-prompt-standard \
+  --order /Users/kangs/github/tamil-prompt-standard/configs/tamil-prompt-engineering-book-quick-test.json \
+  --force \
+  --temp /Users/kangs/Downloads/book \
+  --output /Users/kangs/github/bookbuilder/temp/tamil-prompt-engineering-book-quick-test.pdf \
+  --format=pdf
